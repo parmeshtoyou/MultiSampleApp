db8fc5b4a719e667f964fe700330e3bc748d30c6
parmeshtoyou <parmeshtoyou@gmail.com>
2020-06-23 23:05:19 +0530

    added count down timer fragment

---
+++ app/src/main/java/com/example/multisampleapp/ui/timer/CountDownTimerFragment.kt
@@ -0,0 +1,14 @@
+package com.example.multisampleapp.ui.timer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.multisampleapp.R
+
+class CountDownTimerFragment: Fragment(R.layout.fragment_count_down_timer) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
